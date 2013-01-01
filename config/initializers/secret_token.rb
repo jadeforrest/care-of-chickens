3e861bb31abea7789e2f3be70a82625fe12d0cab
Jade Rubick <jade@newrelic.com>
2013-01-01 07:05:14 -0800

    Add Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chickens::Application.config.secret_token = '7b64f105fc94843deb15d3a1047922d6a55c514a2aa8b3151f00b1f169d251fe0b7fea0c6547fd945c86c8af7bbcc8f8748fbc0f3ddad1e3e459c64b8a3b3a5f'
